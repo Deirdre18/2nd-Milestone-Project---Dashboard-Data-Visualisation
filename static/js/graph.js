09748d667494e2fd8d92f4a9d3c318ecd5915ff2
Deirdre Weldon <deirdreweldon18@gmail.com>
2018-08-19 19:54:23 +0000

    changes to index.html, style.css, deleted graph1.js, graph2.js, graph3.js, graph4.js and combined all graphs in 1 js file (graph.js). Made charts all interactive by using crossfilter for all charts.

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,16 +1,17 @@
 queue()
-        .defer(d3.json, "data/deaths.json")
-        .await(makeGraphs);
+    .defer(d3.json, "data/deaths.json")
+    .await(makeGraphs);
 
     function makeGraphs(error, deathsData) {
-        var ndx = crossfilter(deathsData);
-
-       var parseDate = d3.time.format("%Y").parse;
+	var ndx = crossfilter(deathsData);
+	
+		var parseDate = d3.time.format("%Y").parse;
         deathsData.forEach(function(d){
             d.date = parseDate(d.date);
         });
 
-        var date_dim = ndx.dimension(dc.pluck('date'));
+		
+		var date_dim = ndx.dimension(dc.pluck('date'));
 
         var minDate = date_dim.bottom(1)[0].date;
         var maxDate = date_dim.top(1)[0].date;
@@ -30,10 +31,10 @@ queue()
             }
         });
         
-        
-        var compositeChart = dc.compositeChart('#chart1-here');
+        		
+        var compositeChart = dc.compositeChart("#chart1-here")
         compositeChart
-            .width(700)
+            .width(990)
             .height(200)
             .dimension(date_dim)
             .x(d3.time.scale().domain([minDate, maxDate]))
@@ -49,10 +50,104 @@ queue()
                     .group(home_bInfant_deathsByYear, 'Home_B')
             ])
             .brushOn(false)
-            .render();
+			.render();
+			
+            
 
+		var date_dim = ndx.dimension(dc.pluck('date'));
+        var total_infant_deaths_per_date = date_dim.group().reduceSum(dc.pluck('infant_deaths'));
+
+        var minDate = date_dim.bottom(1)[0].date;
+        var maxDate = date_dim.top(1)[0].date;
+
+		dc.lineChart("#chart2-here")
+            .width(1000)
+            .height(300)
+            .margins({top: 10, right: 50, bottom: 30, left: 50})
+            .dimension(date_dim)
+            .group(total_infant_deaths_per_date)
+            .transitionDuration(500)
+            .x(d3.time.scale().domain([minDate,maxDate]))
+            .xAxisLabel("Year")
+			.yAxisLabel("Infant_Deaths")
+            .yAxis().ticks(4);
+			
+		var gender_dim = ndx.dimension(dc.pluck('gender'));
+		var total_infant_deaths_per_gender = gender_dim.group().reduceSum(dc.pluck('infant_deaths'));
+		
+        dc.pieChart("#chart3-here")
+            .height(365)
+            .radius(90)
+			.transitionDuration(1500)
+            .dimension(gender_dim)
+			.group(total_infant_deaths_per_gender);
+			
+			
+		
+        var name_dim = ndx.dimension(dc.pluck('name'));
+        var total_infant_deaths_per_name = name_dim.group().reduceSum(dc.pluck('infant_deaths'));
+		
+
+        dc.pieChart("#chart4-here")
+            .height(365)
+            .radius(90)
+            .transitionDuration(1500)
+            .dimension(name_dim)
+            .group(total_infant_deaths_per_name);
+			
+			
+		var name_dim = ndx.dimension(dc.pluck('name'));
+
+        var infant_deathsByNameGenderF = name_dim.group().reduceSum(function (d) {
+                if (d.gender === 'F') {
+                    return +d.infant_deaths;
+                } else {
+                    return 0;
+                }
+            });
+        var infant_deathsByNameGenderM = name_dim.group().reduceSum(function (d) {
+                if (d.gender === 'M') {
+                    return +d.infant_deaths;
+                } else {
+                    return 0;
+                }
+            });
+
+        var stackedChart = dc.barChart("#chart5-here")
+        stackedChart
+            .width(475)
+            .height(500)
+            .dimension(name_dim)
+            .group(infant_deathsByNameGenderF, "Female Deaths")
+            .stack(infant_deathsByNameGenderM, "Male Deaths")
+            .x(d3.scale.ordinal())
+            .xUnits(dc.units.ordinal)
+			.yAxisLabel("Infant_Deaths")
+            .legend(dc.legend().x(420).y(0).itemHeight(15).gap(5));
+
+        stackedChart.margins().right = 100;
+		
+		
+        var nameDim = ndx.dimension(dc.pluck('name'));
+        var selectName = dc.selectMenu("#name-selector");
+        genderDim = ndx.dimension(dc.pluck('name'));
+        selectNameGroup = nameDim.group();
+
+        selectName
+            .dimension(nameDim)
+            .group(selectNameGroup);      
+            
+            
+        var nameDim = ndx.dimension(dc.pluck('name'));
+        var infant_deathsByName = nameDim.group().reduceSum(dc.pluck('infant_deaths'));
+        dc.rowChart("#chart6-here")
+            .width(700)
+            .height(330)
+            .dimension(nameDim)
+            .group(infant_deathsByName)
+            .xAxis().ticks(4);    
+
+        
         dc.renderAll();
 		
     }
-	
-	
