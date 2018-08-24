3504b4f082da20353cbc135b4eae7b4979b473ec
Deirdre Weldon <deirdreweldon18@gmail.com>
2018-08-24 11:27:27 +0000

    experimenting with x axis on charts1 & 2 in graph.js

--- static/js/graph.js
+++ static/js/graph.js
@@ -39,7 +39,7 @@ queue()
             .dimension(date_dim)
 			.transitionDuration(250)
             .x(d3.time.scale().domain([minDate, maxDate]))
-            .xAxisLabel("Year")
+            
             .yAxisLabel("Infant_Deaths")
             .legend(dc.legend().x(80).y(20).itemHeight(13).gap(5))
             .renderHorizontalGridLines(true)
@@ -71,9 +71,7 @@ queue()
             .group(total_infant_deaths_per_date)
             .transitionDuration(250)
             .x(d3.time.scale().domain([minDate,maxDate]))
-            .xAxisLabel("Year")
             
-            .xAxis().tickFormat(d3.time.scale().domain([minDate,maxDate]))
            	.yAxisLabel("Infant_Deaths");
             
 			
